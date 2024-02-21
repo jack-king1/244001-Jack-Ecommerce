c9f6170acca97aedac49a6bd8653d61aa66f1e17
jack-king1 <jackking.gm97@gmail.com>
2024-02-21 23:54:19 +0000

    working on category display

--- pawfectecom/src/Components/CatgegoryDisplay.js
+++ pawfectecom/src/Components/CatgegoryDisplay.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function CatgegoryDisplay() {
-    return <div>CatgegoryDisplay</div>;
+function CatgegoryDisplay(props) {
+    return (
+        <div className="bg-primary flex flex-row gap-2 overflow-x-auto PX-20">
+            <div className="min-h-20 min-w-20 md:min-h-60 md:min-w-60 rounded-2xl bg-secondary m-6"></div>
+            <div className="min-h-20 min-w-20 md:min-h-60 md:min-w-60 rounded-2xl bg-secondary m-6"></div>{" "}
+            <div className="min-h-20 min-w-20 md:min-h-60 md:min-w-60 rounded-2xl bg-secondary m-6"></div>{" "}
+            <div className="min-h-20 min-w-20 md:min-h-60 md:min-w-60 rounded-2xl bg-secondary m-6"></div>{" "}
+            <div className="min-h-20 min-w-20 md:min-h-60 md:min-w-60 rounded-2xl bg-secondary m-6"></div>{" "}
+            <div className="min-h-20 min-w-20 md:min-h-60 md:min-w-60 rounded-2xl bg-secondary m-6"></div>{" "}
+            <div className="min-h-20 min-w-20 md:min-h-60 md:min-w-60 rounded-2xl bg-secondary m-6"></div>{" "}
+            <div className="min-h-20 min-w-20 md:min-h-60 md:min-w-60 rounded-2xl bg-secondary m-6"></div>{" "}
+            <div className="min-h-20 min-w-20 md:min-h-60 md:min-w-60 rounded-2xl bg-secondary m-6"></div>
+        </div>
+    );
 }
 
 export default CatgegoryDisplay;
--- pawfectecom/src/Components/Pages/Home.js
+++ pawfectecom/src/Components/Pages/Home.js
@@ -2,13 +2,16 @@ import React from "react";
 import Banner from "../Banner";
 import Image from "../Image";
 import CategoryHeader from "../CategoryHeader";
+import CatgegoryDisplay from "../CatgegoryDisplay";
 
 function Home() {
     return (
         <div className="flex flex-col w-full">
-            <Banner text="✈️Free Delivery on orders over £50!#✈️" />
+            <Banner text="✈️ Free Delivery on orders over £50! ✈️" />
             <Image className="w-full mx-auto" url="./Images/banner.png" />
             <CategoryHeader text="Shop By Category" />
+            <CatgegoryDisplay />
+            <Banner text="✈️ Free Delivery on orders over £50! ✈️" />
         </div>
     );
 }
