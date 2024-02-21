b474384759b8af96484e2e8149b4712258221bf5
jack-king1 <jackking.gm97@gmail.com>
2024-02-21 16:56:29 +0000

    working on navbar styling.

--- pawfectecom/src/Components/Navbar.js
+++ pawfectecom/src/Components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
                 <img className="w-12" src="./Images/logo.png"></img>
             </div>
             <div className="flex-1 text-center w-4/5 justify-center items-center">
-                <div className="flex flex-row justify-center items-center">
+                <div className="flex flex-row justify-center items-center w-full">
                     <input
                         className="px-2 rounded-xl border border-black h-1/2 text-sm"
                         placeholder="Search..."
@@ -19,9 +19,11 @@ function Navbar() {
             </div>
             <div className="flex flex-row gap-4 items-center">
                 <CiUser className="text-black text-2xl" />
-                <div className="">
+                <div className="relative">
                     <LuShoppingBasket className="text-black text-xl" />
-                    <div className="">3</div>
+                    <div className="bg-red-500 text-white text-center rounded-full w-3 h-3 absolute -bottom-1 -right-0.5 flex items-center justify-center">
+                        <div className="text-xs text-white">3</div>
+                    </div>
                 </div>
 
                 <FaBars className="text-black" />
