de44ffd19165140d493a7508f113f884250d1b2e
jack-king1 <jackking.gm97@gmail.com>
2024-02-21 16:44:24 +0000

    working on navbar layout

--- pawfectecom/src/Components/Navbar.js
+++ pawfectecom/src/Components/Navbar.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { FaBars } from "react-icons/fa6";
+import { LuShoppingBasket } from "react-icons/lu";
+import { CiUser } from "react-icons/ci";
 
 function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <div className="flex flex-row justify-between px-4 py-3 items-center">
+            <div>
+                <img className="w-12" src="./Images/logo.png"></img>
+            </div>
+            <div className="flex-1 text-center w-4/5 justify-center items-center">
+                <div className="flex flex-row justify-center items-center">
+                    <input
+                        className="px-2 rounded-xl border border-black h-1/2 text-sm"
+                        placeholder="Search..."
+                    ></input>
+                </div>
+            </div>
+            <div className="flex flex-row gap-4 items-center">
+                <CiUser className="text-black text-2xl" />
+                <div className="">
+                    <LuShoppingBasket className="text-black text-xl" />
+                    <div className="">3</div>
+                </div>
+
+                <FaBars className="text-black" />
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
--- pawfectecom/tailwind.config.js
+++ pawfectecom/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#26d07c",
+                secondary: "#d0267a",
+            },
+        },
     },
     plugins: [],
 };
