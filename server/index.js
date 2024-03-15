2ee76765366f35639d30eae959b0216953d752c7
jack-king1 <jackking.gm97@gmail.com>
2024-03-15 10:11:24 +0000

    cleaned up redundant server functions and added .env variables

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-//const sql = require("mssql");
+require("dotenv").config();
 const sql = require("mssql");
 const express = require("express");
 const app = express();
@@ -25,10 +25,10 @@ app.use(cors());
 // };
 
 const sqlConfig = {
-    server: "52.151.88.124",
+    server: process.env.DB_HOST,
     database: "jackkingEcomDB",
     user: "jack",
-    password: "password123!!",
+    password: process.env.DB_PASS,
     pool: {
         min: 0,
         max: 10,
@@ -44,81 +44,6 @@ sql.connect(sqlConfig, function (err) {
     if (err) console.log(err);
     else console.log("Connected!");
 });
-
-app.get("/", (req, res) => {
-    res.send("Default root found!");
-});
-
-app.get("/getitem", (req, res) => {
-    res.send("getting item!");
-});
-
-//get item id from
-app.get("/getitem/:itemid", (req, res) => {
-    const itemid = req.params.videoid;
-    res.send(`getting item: ${itemid}`);
-    console.log(req);
-});
-
-//multiply number using routes and params inside the url.
-app.get("/multiply/:numberone&:numbertwo", (req, res) => {
-    const n1 = req.params.numberone;
-    const n2 = req.params.numbertwo;
-    res.send(`${n1} x ${n2}: ${n1 * n2}`);
-});
-
-//http://localhost:3000/multiplyquery?numberone=1&numbertwo=4
-//%20 adds a space in a URL.
-//using routes rather
-app.get("/employees/", (req, res) => {
-    const n1 = req.query.numberone;
-    const n2 = req.query.numbertwo;
-    const employeename = req.query.name;
-    res.send(`${n1} x ${n2}: ${n1 * n2} Name: ${employeename}`);
-});
-
-//passing params into the req query
-app.get("/employees", (req, res) => {
-    const emplpoyeeid = req.query.employeeid;
-    res.send(`Employees rout: ${emplpoyeeid}`);
-});
-
-//get country code using query e.g. http://localhost:3000/countries/?countryCode1=UK&countryCode2=AR
-app.get("/countries/", async (req, res) => {
-    const countryCode1 = req.query.countryCode1;
-    const countryCode2 = req.query.countryCode2;
-    try {
-        await sql.connect(sqlConfig);
-        let request = new sql.Request();
-        request.input("country_id1", sql.VarChar, countryCode1);
-        request.input("country_id2", sql.VarChar, countryCode2);
-        const query =
-            "SELECT * FROM countries WHERE country_id = @country_id1 OR country_id = @country_id2";
-        const result = await request.query(query);
-        res.json(result);
-    } catch (err) {
-        console.log(err);
-        res.status(500);
-    }
-});
-
-//country ID using params where country e.g. http://localhost:3000/countries/UK
-app.get("/countries/:countryId", async (req, res) => {
-    const countryCode = req.params.countryId;
-    try {
-        await sql.connect(sqlConfig);
-        let request = new sql.Request();
-        request.input("country_id2", sql.VarChar, countryCode);
-        const query = "SELECT * FROM countries WHERE country_id = @country_id2";
-        const result = await request.query(query);
-        res.json(result);
-    } catch (err) {
-        console.log(err);
-        res.status(500);
-    }
-});
-
-//post data to database
 
 //fetch all products as json to display on prodyct page.
 app.get("/products", async (req, res) => {
@@ -169,19 +94,6 @@ app.get("/products/:productid/highlights", async (req, res) => {
     } catch (err) {
         console.log(err);
         res.status(500);
-    }
-});
-
-app.delete("/deleteproduct/:productId", async (req, res) => {
-    const productId = req.params.productId;
-    try {
-        await sql.connect(sqlConfig);
-        let request = new sql.Request();
-        request.input("productId", sql.Int, productId);
-        let query = "DELETE...";
-        const result = request.query();
-    } catch (err) {
-        console.log(err);
     }
 });
 
